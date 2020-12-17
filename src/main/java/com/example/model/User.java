766731a7a7e5b4c102520299167a5f5b2b3a70ce
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-18 01:52:33 +0700

    Update User.java

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,15 +1,43 @@
 package com.example.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.sql.Date;
 
+import javax.validation.constraints.*;
+
 public class User {
+
+    /*
+    All these annotations come from the javax.validation.constraints package.
+    For the complete list of Java Bean Validation annotations, see
+    https://javaee.github.io/javaee-spec/javadocs/javax/validation/constraints/package-summary.html
+    */
+
+    @Size(min = 3, max= 50)
     private String name;
+
+    @NotBlank
+    @Email(message = "Please enter a valid e-mail address")
     private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 15)
     private String password;
+
+    @NotBlank
     private String gender;
+
+    @Size(max = 100)
     private String note;
+
+    @AssertTrue
     private boolean married;
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
     private Date birthday;
+
+    @NotBlank
     private String profession;
 
     @Override
