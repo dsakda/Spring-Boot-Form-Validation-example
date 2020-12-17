88e9abf02bb66ff84bed62a01601510d87e777ce
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-18 01:52:30 +0700

    Update MainController.java

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -3,8 +3,13 @@ package com.example.controller;
 import com.example.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+import java.sql.Date;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,5 +22,16 @@ public class MainController {
         model.addAttribute("listProfession", listProfession);
         model.addAttribute("user", new User());
         return "register_form";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+        System.out.println(user);
+
+        if (bindingResult.hasErrors()) {
+            return "register_form";
+        } else {
+            return "register_success";
+        }
     }
 }
